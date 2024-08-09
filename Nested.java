a7da9e2ad5460fc8417de1f7f3c3c78f882e181b
Soham0274 <scsawant09@gmail.com>
2024-08-09 14:09:43 +0530

    Add files via upload

---
+++ Nested.java
@@ -0,0 +1,29 @@
+class Conditional
+{
+	public static void main (String args[])
+	{
+	 int marks = 80;
+	 if (marks<90)
+ 	{ 
+  	if (marks <=80)
+     		{
+         		System.out.println("Excellent");
+     		} 
+  	else if (marks <=70)
+      		{
+  			System.out.println("Good");
+		}
+	else
+		{
+ 			System.out.println("Average");
+		}
+	}
+	else
+	{
+		if(marks<=60);
+	{	
+		System.out.println("Satisfactory");
+		}
+		}
+	}
+}
